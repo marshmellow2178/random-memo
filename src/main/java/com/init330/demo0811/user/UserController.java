c6dce4ec9460ae594e8b0ab73a5fffaba8f81f98
marshmellow2178 <115971843+marshmellow2178@users.noreply.github.com>
2025-10-05 15:16:22 +0900

    gradle 의존성 오류해결

--- src/main/java/com/init330/demo0811/user/UserController.java
+++ src/main/java/com/init330/demo0811/user/UserController.java
@@ -7,6 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
